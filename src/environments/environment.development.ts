649ce7f67b3041c311eeb88a2a90ac9c015d93bc
Luis Felipe Garcia <fellgar246@gmail.com>
2024-12-15 13:14:37 -0600

    delete non confidential env info

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseUrl: `https://api.github.com/repos/angular/angular`,
-  gitHubToken: `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`,
+  gitHubToken: ``,
 };
